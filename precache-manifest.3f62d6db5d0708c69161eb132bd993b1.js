90f201aae2134cd66feab4c8b9c61319cf0d790f
JeJn <leejj1082@icloud.com>
2021-07-26 16:45:01 +0900

    Updates

---
+++ precache-manifest.3f62d6db5d0708c69161eb132bd993b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fedc81342bd11a607123dd0032c3684",
+    "url": "/jeju_mobile_develop/index.html"
+  },
+  {
+    "revision": "a938dc1b769d9f6aa8d8",
+    "url": "/jeju_mobile_develop/static/css/2.40c68a69.chunk.css"
+  },
+  {
+    "revision": "b1887315feece96a5e34",
+    "url": "/jeju_mobile_develop/static/css/main.ea88f1c1.chunk.css"
+  },
+  {
+    "revision": "a938dc1b769d9f6aa8d8",
+    "url": "/jeju_mobile_develop/static/js/2.6409ee12.chunk.js"
+  },
+  {
+    "revision": "0dc713a05373e5c9a88cb999f31ae6e4",
+    "url": "/jeju_mobile_develop/static/js/2.6409ee12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1887315feece96a5e34",
+    "url": "/jeju_mobile_develop/static/js/main.e35c7b75.chunk.js"
+  },
+  {
+    "revision": "1ec63adf0d52fcf2aa65",
+    "url": "/jeju_mobile_develop/static/js/runtime-main.7b07329f.js"
+  }
+]);
